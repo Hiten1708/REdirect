45157890b275bd104590933e665bb1377e64cf0e
Hiten1708 <68720970+Hiten1708@users.noreply.github.com>
2023-05-23 01:21:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
 
 function App() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://ipapi.co/json'
+
+        // Just in case I want to add headers
+            // , {
+            //   method: 'GET',
+            //   headers: {
+            //     'X-RapidAPI-Key': '617613e6b7msh38a292b67a972c1p1fc264jsn5c45f98f2a6b',
+            //     'X-RapidAPI-Host': 'ip-geo-location.p.rapidapi.com'
+            //   }}
+        );
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+          // Redirect to the VSCO website
+          // window.location.href = 'https://vsco.co/hiten1409/gallery';
+        } else {
+          throw new Error('API request failed');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+    }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
